3617c36c7ca45bc99aed8fcfb124d7cf278b86e7
aldifhrent <aldifahriziilmawan@gmail.com>
2025-11-06 16:24:11 +0700

    fix: add flex-wrap to sidebar for better layout

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function SideBar() {
   return (
-    <nav className="flex flex-row lg:flex-col gap-2">
+    <nav className="flex flex-wrap flex-row lg:flex-col gap-2">
       {menus
         .map((m) => (
           <Link key={m.href} href={m.href} className="text-black hover:underline hover:text-blue-700">
