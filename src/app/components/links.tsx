8adfeda424d9b9bd886c738dca239d2ccd43e1eb
aldifhrent <aldifahriziilmawan@gmail.com>
2025-11-06 15:51:57 +0700

    add: target blanks for contact url

--- src/app/components/links.tsx
+++ src/app/components/links.tsx
@@ -3,10 +3,34 @@ export default function Contacts() {
     <div className="mt-3">
       <p className="font-bold">Links</p>
       <div className="mt-2 flex flex-col gap-3 text-">
-        <a href="https://www.linkedin.com/in/aldifahrizi" className="text-sm text-blue-600 hover:underline">LinkedIn</a>
-        <a href="https://github.com/aldifhrent" className="text-sm text-blue-600 hover:underline">Github</a>
-        <a href="https://www.instagram.com/aldi.fhrn/" className="text-sm text-blue-600 hover:underline">Instagram</a>
-        <a href="https://wa.me/+6282218718161" className="text-sm text-blue-600 hover:underline">Phone Number</a>
+        <a
+          href="https://www.linkedin.com/in/aldifahrizi"
+          className="text-sm text-blue-600 hover:underline"
+          target="blank"
+        >
+          LinkedIn
+        </a>
+        <a
+          href="https://github.com/aldifhrent"
+          className="text-sm text-blue-600 hover:underline"
+          target="blank"
+        >
+          Github
+        </a>
+        <a
+          href="https://www.instagram.com/aldi.fhrn/"
+          className="text-sm text-blue-600 hover:underline"
+          target="blank"
+        >
+          Instagram
+        </a>
+        <a
+          href="https://wa.me/+6282218718161"
+          className="text-sm text-blue-600 hover:underline"
+          target="blank"
+        >
+          Phone Number
+        </a>
       </div>
     </div>
   );
